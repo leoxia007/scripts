5aa292e8a75bc6bca273234fe90f7359fff6b432
leoxia007 <xialei198785@gmail.com>
2019-04-30 09:50:42 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 OUT=out/target/product/mha;
 cp -f $OUT/mha.json api/mha.json;
-rsync -avP -e ssh $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/MHA/;
+rsync -avP -e ssh $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/MHA/
+while [ $? -ne 0 ]
+do
+rsync -avP -e ssh $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/MHA/
+done
 touch gittemp.txt;
 basename -s .zip $(ls $OUT/RR-P*.zip) > gittemp.txt;
 cd api;
@@ -9,4 +13,5 @@ git add mha.json;
 git commit -F ../gittemp.txt ;
 git push;
 cd ..;
-rm -f gittemp.txt
+rm -f gittemp.txt;
+echo "ROM Upload Done!"
