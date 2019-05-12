6e4a29dd6c67055419058b6c6dbd04ed9d914df6
leoxia007 <xialei198785@gmail.com>
2019-05-12 12:29:54 +0800

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-OUT=out/target/product/mha;
-cp -f $OUT/mha.json api/mha.json;
-rsync -avP -e ssh $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/MHA/
+VAR=$1
+OUT=out/target/product/$VAR;
+rsync -avP -e "ssh -o 'ProxyCommand nc -X 5 -x 127.0.0.1:1080 %h %p'" $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/$(echo $VAR| tr '[a-z]' '[A-Z]')/
 while [ $? -ne 0 ]
 do
-rsync -avP -e ssh $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/MHA/
+rsync -avP -e "ssh -o 'ProxyCommand nc -X 5 -x 127.0.0.1:1080 %h %p'" $(ls $OUT/RR-P*.zip) xlagjn@frs.sourceforge.net:/home/frs/project/xlagjn-rr/$(echo $VAR| tr '[a-z]' '[A-Z]')/
 done
+cp -f $OUT/$VAR.json api/$VAR.json;
 touch gittemp.txt;
 basename -s .zip $(ls $OUT/RR-P*.zip) > gittemp.txt;
 cd api;
-git add mha.json;
+git add $VAR.json;
 git commit -F ../gittemp.txt ;
 git push;
 cd ..;
